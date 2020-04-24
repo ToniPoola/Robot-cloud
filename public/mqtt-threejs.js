64f92ba889b5fc3f83d3c7402e7ea8f99e3abce7
Toni Poola <Toni.Poola@seamk.fi>
2020-04-24 12:37:03 +0300

    commit

--- public/mqtt-threejs.js
+++ public/mqtt-threejs.js
@@ -19,6 +19,19 @@ camera.lookAt(scene.position);
 renderer.setSize(width, height);
 three_view.appendChild(renderer.domElement);
 
+// plane
+{
+    let geometry = new THREE.PlaneBufferGeometry(40, 40);
+    let material = new THREE.MeshPhongMaterial({
+        color: 0xAAAAAA,
+        specular: 0x101010
+    });
+    let mesh = new THREE.Mesh(geometry, material);
+    mesh.position.z = -1;
+    scene.add(mesh);
+}
+
+
 // TEST CUBE
 {
     let geometry = new THREE.BoxGeometry(1, 1, 1);
@@ -26,15 +39,37 @@ three_view.appendChild(renderer.domElement);
         color: 0xff0000
     });
     let mesh = new THREE.Mesh(geometry, material);
-    //scene.add(mesh);
+    scene.add(mesh);
 }
 
 let light = new THREE.DirectionalLight(0xAAAAAA, 1.5);
 light.position.x = 10;
 light.position.y = 10;
 light.position.z = 10;
-light.lookAt = new THREE.Vector3(0,0,0); // scene.position
+light.lookAt = (scene.position); 
 scene.add(light);
+
+const stl_loader = new THREE.STLLoader();
+const load_stl = (url) => {
+    return new Promise((resolve) => {
+        stl_loader.load(url, resolve);
+    });
+};
+
+const load_geometries = async () => {
+    let geometry = await load_stl('./FANUC_R2000iA165F-STL');
+    let mesh = new THREE.Mesh(geometry, dark);
+    mesh.geometry.scale(0.001,0.001,0.001);
+    scene.add(mesh);
+};
+
+let dark = new THREE.MeshLambertMaterial({
+    color: 0x111111
+});
+
+let yellow = new THREE.MeshLambertMaterial({
+    color: 0xFFFF00
+});
 
 
 const animate = () => {
